851f493f47bca0f00d0ce6fde1628b48d6b35b1b
anoneyemous <anoneyemous@gmail.com>
2021-05-26 09:59:03 +0200

    Add outcomes if no characters or bosses are selected.

--- randomize.js
+++ randomize.js
@@ -4,23 +4,32 @@ function randomize(){
 
   var submit = document.getElementById("submit");
 
+  // Character selection
   var chosen = [];
-
   for (var i = 0; i < characters.length; i++) {
     if (characters[i].checked) chosen.push(characters[i].value);
   }
+  if (Object.keys(chosen).length === 0) {
+    var random_char = "any character you want"; }
+  else {
+    var random_char = chosen[Math.floor(Math.random() * chosen.length)];
+  }
 
-  var random_char = chosen[Math.floor(Math.random() * chosen.length)];
-
+  // Boss selection
   var chosen = [];
-
   for (var i = 0; i < bosses.length; i++) {
     if (bosses[i].checked) chosen.push(bosses[i].value);
   }
+  if (Object.keys(chosen).length === 0) {
+    var random_boss = "any final boss you want"; }
+  else {
+    var random_boss = chosen[Math.floor(Math.random() * chosen.length)];
+  }
 
-  var random_boss = chosen[Math.floor(Math.random() * chosen.length)];
+  // Printing text
   var next_run = "<h1>Try using <b>" + random_char + "</b> to defeat <b>" + random_boss + "</b>!</h1>";
 
+  // Selecting pictures
   var boss_image = random_boss.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
   var char_image = random_char.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
